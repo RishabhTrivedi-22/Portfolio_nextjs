7564f36321b9082fb77dea2635d3cb581ca93eb0
RishabhTrivedi-22 <rishabhtrivedi695@gmail.com>
2025-05-26 15:01:43 +0530

    All animations finished.

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,6 +1,6 @@
+"use client";
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
-import StarIcon from "@/assets/icons/star.svg";
 import bookImage from "@/assets/images/book-cover.png";
 import Image from "next/image";
 import JavascriptIcon from "@/assets/icons/square-js.svg";
@@ -9,12 +9,12 @@ import CssIcon from "@/assets/icons/css3.svg";
 import ReactIcon from "@/assets/icons/react.svg";
 import ChromeIcon from "@/assets/icons/chrome.svg";
 import GitIcon from "@/assets/icons/github.svg";
-import { TechIcon } from "@/components/TechIcon";
 import mapImage from "@/assets/images/map.png";
 import smileMemoji from "@/assets/images/memoji-smile.png";
 import { CardHeader } from "@/components/CardHeader";
 import { ToolBoxItem } from "@/components/ToolBoxItem";
-// import css from "styled-jsx/css";
+import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const toolBoxItems = [
   {
@@ -88,6 +88,8 @@ const hobbies = [
 ];
 
 export const AboutSection = () => {
+  const constraintRef = useRef(null);
+
   return (
     <section id="About" className="py-16 lg:py-28">
       <div className="container">
@@ -98,55 +100,73 @@ export const AboutSection = () => {
         />
         <div className="mt-20 flex flex-col gap-8">
           <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
-          <Card className="h-[320px] md:col-span-2 lg:col-span-1">
-            <CardHeader title="My Reads" description="Explore the books shaping my perspective" />
-            <div className="w-40 mx-auto mt-2 md:mt-0">
-              <Image src={bookImage} alt="Book cover" />
-            </div>
-          </Card>
-          <Card className="p-0 md:col-span-3 lg:col-span-2">
-            <CardHeader
-              title="My ToolBox"
-              description="Explore the technologies and tools I use to craft exceptional digital experiences"
-            />
-            <ToolBoxItem tool={toolBoxItems} />
-            <ToolBoxItem 
-              tool={toolBoxItems} 
-              className="mt-6"
-              toolWrapperClassName="-translate-x-1/2"
-            />
-          </Card>
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+              <CardHeader
+                title="My Reads"
+                description="Explore the books shaping my perspective"
+              />
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image src={bookImage} alt="Book cover" />
+              </div>
+            </Card>
+            <Card className="p-0 md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="My ToolBox"
+                description="Explore the technologies and tools I use to craft exceptional digital experiences"
+              />
+              <ToolBoxItem
+                tool={toolBoxItems}
+                toolWrapperClassName="animate-move-left [animation-duration:30s]"
+              />
+              <ToolBoxItem
+                tool={toolBoxItems}
+                className="mt-6"
+                toolWrapperClassName="-translate-x-1/2 animate-move-right [animation-duration:15s]"
+              />
+            </Card>
           </div>
           <div className="grid grid-cols-1 gap-8 md:grid-cols-5">
-          <Card className="flex flex-col md:col-span-3">
-            <CardHeader
-              title="Beyond the Code"
-              description="Explore my interests and hobbies beyond the digital realm."
-            />
-            <div className="relative flex-1">
-              {hobbies.map((hobby) => (
-                <div 
-                  key={hobby.title} 
-                  className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
-                  style={{
-                    left: hobby.left,
-                    top: hobby.top,
-                  }}
-                >
-  
-                    <span className="font-medium text-gray-950">{hobby.title}</span>
+            <Card className="flex flex-col md:col-span-3">
+              <CardHeader
+                title="Beyond the Code"
+                description="Explore my interests and hobbies beyond the digital realm."
+              />
+              <div className="relative flex-1" ref={constraintRef}>
+                {hobbies.map((hobby) => (
+                  <motion.div
+                    key={hobby.title}
+                    className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}
+                    drag
+                    dragConstraints={constraintRef}
+                  >
+                    <span className="font-medium text-gray-950">
+                      {hobby.title}
+                    </span>
                     <span>{hobby.emoji}</span>
-                  
-                </div>
-              ))}
-            </div>
-          </Card>
-          <Card className="relative md:col-span-2">
-            <Image src={mapImage} alt="map" className="w-full h-full object-cover object-left-top" />
-            <div className="absolute top-1/2 left-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:outline after:outline-2 after:inset-0 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
-            <Image src={smileMemoji} alt="memoji-smiling" className="size-20 " />
-            </div>
-          </Card>
+                  </motion.div>
+                ))}
+              </div>
+            </Card>
+            <Card className="relative md:col-span-2 h-[320px] p-0">
+              <Image
+                src={mapImage}
+                alt="map"
+                className="w-full h-full object-cover object-left-top"
+              />
+              <div className="absolute top-1/2 left-1/2 translate-x-1/2  size-20 rounded-full after:content-[''] after:absolute after:outline after:outline-2 after:inset-0 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
+                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
+                <Image
+                  src={smileMemoji}
+                  alt="memoji-smiling"
+                  className="size-20 "
+                />
+              </div>
+            </Card>
           </div>
         </div>
       </div>
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -7,36 +7,37 @@ import { SectionHeader } from "@/components/SectionHeader";
 import GrainImg from "@/assets/images/grain.jpg";
 import Image from "next/image";
 import { Card } from "@/components/Card";
+import { Fragment } from "react";
 
 const testimonials = [
   {
     name: "Alex Turner",
     position: "Marketing Manager @ TechStartups",
-    text: "Alex was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
+    text: "Rishabh was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
     avatar: memojiAvatar1,
   },
   {
     name: "Olivia Green",
     position: "Head of Design @ GreenLeaf",
-    text: "Working with Alex was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
+    text: "Working with Rishabh was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
     avatar: memojiAvatar2,
   },
   {
     name: "Daniel White",
     position: "CEO @ InnovateCo",
-    text: "Alex's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
+    text: "Rishabh's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
     avatar: memojiAvatar3,
   },
   {
     name: "Emily Carter",
     position: "Product Manager @ GlobalTech",
-    text: "Alex is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
+    text: "Rishabh is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
     avatar: memojiAvatar4,
   },
   {
     name: "Michael Brown",
     position: "Director of IT @ MegaCorp",
-    text: "Alex's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
+    text: "Rishabh's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
     avatar: memojiAvatar5,
   },
 ];
@@ -51,10 +52,12 @@ export const TestimonialsSection = () => {
           description="Dont't just take my word for it. See what my clients have to say about my work."
         />
 
-        <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex gap-8 flex-none">
-            {testimonials.map((testimonial) => (
-              <Card key={testimonial.name} className="p-6 h-auto max-w-xs md:max-w-md md:p-8">
+        <div className="mt-12 lg:mt-20 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex gap-8 flex-none pr-8 py-4 animate-move-left [animation-duration:60s] hover:[animation-play-state:paused]">
+            {[...new Array(2)].fill(0).map((_, idx) => (
+              <Fragment key={idx}>
+                {testimonials.map((testimonial) => (
+              <Card key={testimonial.name} className="p-6 h-auto max-w-xs md:max-w-md md:p-8 hover:-rotate-3 transition duration-300">
                 <div className="flex items-center gap-4">
                   <div className="inline-flex justify-center items-center size-14 bg-gray-700 rounded-full flex-shrink-0">
                     <Image
@@ -74,6 +77,8 @@ export const TestimonialsSection = () => {
                 </div>
                 <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
               </Card>
+            ))}
+              </Fragment>
             ))}
           </div>
         </div>
